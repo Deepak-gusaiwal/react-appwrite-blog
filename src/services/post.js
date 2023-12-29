c3461f583bc4b20f14500a0609a252e3454e4137
deepak <dgusaiwal537@gmail.com>
2023-12-29 22:42:52 +0530

    start post service

---
+++ src/services/post.js
@@ -0,0 +1,13 @@
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+class PostService {
+    client = new Client;
+    databases;
+    bucket;
+
+    constructor(){
+        // this.client = 
+    }
+}
+
+const postServie = new PostService();
